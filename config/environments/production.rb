21654d5e37263ece7f31dba7d906db6ef26f6a4c
wandji20 <wbertrand10@gmail.com>
2022-01-20 23:37:26 +0100

    Add procfile and email config settimggs in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,24 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "email_sender_api_production"
 
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  host = ENV['APP_URL'] #replace with your own url
+  config.action_mailer.default_url_options = { host: host }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV['APP_USERNAME'],
+    password: ENV['APP_PASSWORD'],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
